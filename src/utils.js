b018dfacb00b9265c991f337c667a90391db499e
GustavoChavarria <gchava92@gmail.com>
2020-02-25 16:22:31 -0600

    refactor

---
+++ src/utils.js
@@ -0,0 +1,200 @@
+/* jslint browser */
+/* global window */
+/* global document */
+
+import { svgAsPngUri } from 'save-svg-as-png';
+import { saveAs } from 'file-saver';
+
+const DEFAULT_OPTIONS = {
+  filename: 'Image',
+  forceFixText: false,
+  printDate: true
+};
+
+let ORIGINAL_PADDING = null;
+
+/**
+ *
+ * @param {Document} node
+ */
+const setTemporalPadding = node => {
+  ORIGINAL_PADDING = node.style.padding;
+  node.style.padding = '3px';
+};
+
+/**
+ *
+ * @param {Document} node
+ */
+const revertPadding = node => {
+  node.style.padding = ORIGINAL_PADDING;
+};
+
+/**
+ *  Merge default options with user options
+ *
+ * @param {*} userOptions
+ */
+const getOptions = userOptions => {
+  return {
+    ...DEFAULT_OPTIONS,
+    ...userOptions
+  };
+};
+
+/**
+ * Set computed style in static style of svg element
+ * @param {Document} el
+ */
+const fixColorSvg = el => {
+  const { color } = window.getComputedStyle(el);
+
+  el.style.color = color;
+};
+
+/**
+ * Set computed style in static style of svg element
+ * @param {Document} el
+ */
+const fixSizeSvg = el => {
+  const styles = window.getComputedStyle(el);
+
+  el.style.width = styles.width;
+  el.style.height = styles.height;
+};
+
+/**
+ * Fix all text with the class "fixed-text"
+ * @param {Document} node
+ */
+const fixText = node => {
+  hardFixText(node, ['.fixed-text']);
+};
+
+/**
+ *
+ * @param {Document} node
+ * @param {Array} seek
+ */
+const hardFixText = (
+  node,
+  seek = [
+    'b',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'i',
+    'mark',
+    'p',
+    'small',
+    'strong'
+  ]
+) => {
+  const allTexts = node.querySelectorAll(seek);
+
+  for (const text of allTexts) {
+    text.style.fontFamily = window.getComputedStyle(text).fontFamily;
+    text.style.fontSize = window.getComputedStyle(text).fontSize;
+    text.style.fontWeight = window.getComputedStyle(text).fontWeight;
+    text.style.width = window.getComputedStyle(text).width;
+  }
+};
+
+/**
+ * Little hack - replace svgs with icons in png
+ * @param {Document} svgs
+ */
+const replaceFontAwesomeIconsWithImages = async node => {
+  const svgs = node.querySelectorAll('svg');
+
+  const images = [];
+
+  for (const item of svgs) {
+    const itemAttribute = item.getAttribute('data-icon');
+    const cache = images.find(img => img.dataIcon === itemAttribute);
+
+    const imgElement = document.createElement('img');
+
+    if (cache) {
+      imgElement.src = cache.uri;
+    } else {
+      /* eslint-disable no-await-in-loop */
+
+      const uri = await svgAsPngUri(item, 'icon.png');
+      imgElement.src = uri;
+      images.push({ dataIcon: itemAttribute, uri });
+
+      /* eslint-enable no-await-in-loop */
+    }
+
+    imgElement.dataset.icon = itemAttribute;
+    imgElement.width = item.clientWidth;
+    imgElement.height = item.clientHeight;
+
+    // Const parent = item.parentNode;
+    item.replaceWith(imgElement);
+  }
+};
+
+/**
+ *
+ * @param {Document} node
+ * @param {Boolean} forceFixText
+ */
+const applyFixs = (node, forceFixText = false) => {
+  const svgs = node.querySelectorAll('svg');
+
+  fixText(node);
+
+  if (forceFixText) {
+    hardFixText(node);
+  }
+
+  for (const el of svgs) {
+    fixColorSvg(el);
+    fixSizeSvg(el);
+  }
+};
+
+const getFileName = options => {
+  if (options.printDate) {
+    const date = new Date().toDateString();
+
+    return `${options.filename} (${date})`;
+  }
+
+  return options.filename;
+};
+
+/**
+ *
+ * @param {*} node
+ * @param {*} callback
+ */
+export const scaffolding = async (node, userOptions, callback) => {
+  console.log('scaffolding');
+
+  const options = getOptions(userOptions);
+  let canvas = null;
+
+  applyFixs(node, options.forceFixText);
+
+  setTemporalPadding(node);
+
+  try {
+    canvas = await callback();
+  } catch {
+    /* Litte hack because not working on safari */
+    await replaceFontAwesomeIconsWithImages(node);
+    await callback();
+
+    canvas = await callback();
+  }
+
+  revertPadding(node);
+
+  saveAs(canvas, `${getFileName(options)}.png`);
+};
